e65a684da2a6bc4fdad38e9c70dd0c43c3b2fc9d
Yannis Biasutti <yannis@biasutti.ch>
2019-10-01 19:07:19 +0200

    Placed Login link

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <header>
-    <div id="logo"></div>
-    <div class="w3-right w3-col">Login</div>
+    <div id="logo" class="header-left w3-col s3"></div>
+    <div class="header-middle w3-col s6"></div>
+    <div class="header-right w3-col s3 w3-right-align w3-padding-large">Login</div>
 </header>
