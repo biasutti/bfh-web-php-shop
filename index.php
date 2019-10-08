2ac83eddd296e1ddf63c34ed76a796c8fc9f9787
Yannis Biasutti <yannis.biasutti@students.bfh.ch>
2019-10-08 18:50:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include "./includes/enum/pages.php"
             </div>
         </div>
         <div class="content w3-col s9">
-            <p><?php echo "Hello World" ?> Jenkins do stuff!</p>
+            <p><?php echo "Hello World" ?> Jenkins do stuff!!!</p>
         </div>
     </main>
     <?php
