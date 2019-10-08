a5ae6d0f106f75a3bec54c6489b7363567342b28
Yannis <yannis@biasutti.ch>
2019-10-08 22:36:05 +0200

    Added link to Login in header navigation

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,8 @@
 <header class="flex-container">
     <div id="logo" class="flex-item flex-size-3">LOGO</div>
-    <div class="flex-item-1 flex-size-1">Login</div>
+    <ul class="navigation-header flex-item flex-size-1">
+        <li class="">
+            <a href="#">Login</a>
+        </li>
+    </ul>
 </header>
