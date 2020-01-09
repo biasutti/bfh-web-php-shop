f01ddef724c7aa009f1235a0fee68397ce9c6189
barts5 <barts5@bfh.ch>
2020-01-09 10:34:30 +0100

    changed impressum to work also with cookies

--- pages/impressum.php
+++ pages/impressum.php
@@ -1,6 +1,14 @@
 <?php
-if($_GET["lang"]=="de"){
-  include("./pages/impressum_de.php");
-}elseif($_GET["lang"]=="fr"){
-  include("./pages/impressum_fr.php");
+if(isset($_GET["lang"])){
+  if($_GET["lang"]=="de"){
+    include("./pages/impressum_de.php");
+  }elseif($_GET["lang"]=="fr"){
+    include("./pages/impressum_fr.php");
+  }
+}else{
+  if($_COOKIE["lang"]=="de"){
+    include("./pages/impressum_de.php");
+  }elseif($_COOKIE["lang"]=="fr"){
+    include("./pages/impressum_fr.php");
+  }
 }
